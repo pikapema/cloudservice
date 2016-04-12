cd4e75d7f4b08bb4acd8684076299367d9dbae61
Katarina Peltola <katarina.p.magnusson@hotmail.com>
2016-04-12 08:12:48 +0200

    Added example project for Cloudservice with service bus

---
+++ FrontEndWebRole/QueueConnector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+using Microsoft.WindowsAzure;
+
+namespace FrontendWebRole
+{
+    public static class QueueConnector
+    {
+        // Thread-safe. Recommended that you cache rather than recreating it
+        // on every request.
+        public static QueueClient OrdersQueueClient;
+
+        // The name of your queue.
+        public const string QueueName = "OrdersQueue";
+
+        public static NamespaceManager CreateNamespaceManager()
+        {
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            return NamespaceManager.CreateFromConnectionString(connectionString);
+
+        }
+
+        public static void Initialize()
+        {
+            // Using Http to be friendly with outbound firewalls.
+            ServiceBusEnvironment.SystemConnectivity.Mode =
+                ConnectivityMode.Http;
+
+
+            var namespaceManager = CreateNamespaceManager();
+
+            if (!namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.CreateQueue(QueueName);
+            }
+
+
+            // Get a client to the queue.
+            var messagingFactory = MessagingFactory.Create(
+                namespaceManager.Address,
+                namespaceManager.Settings.TokenProvider);
+            OrdersQueueClient = messagingFactory.CreateQueueClient(
+                QueueName);
+        }
+    }
+}
---
+++ FrontEndWebRole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrontEndWebRole.Startup))]
+namespace FrontEndWebRole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
